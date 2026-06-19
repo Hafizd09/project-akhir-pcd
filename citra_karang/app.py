d825a3296eb6de758757359ca11258ba3f45e740
Hafizd09 <mhafizd992@gmail.com>
2026-06-19 23:21:49 +0700

    update konfigurasi vercel dan app

--- citra_karang/app.py
+++ citra_karang/app.py
@@ -166,3 +166,12 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+    from flask import Flask
+app = Flask(__name__)
+
+# ... kode route Anda ...
+
+# Bagian ini opsional untuk lokal, tapi Vercel akan menembak variabel `app` di atas
+if __name__ == "__main__":
+    app.run(debug=True)
